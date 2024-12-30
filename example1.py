7075e9e34fad08be032aa986bc63622a53901d80
GeorgeDealy <49003850+GeorgeDealy@users.noreply.github.com>
2024-12-30 18:01:52 -0500

    Adding the 4th line
    4th line added

--- example1.py
+++ example1.py
@@ -1,3 +1,4 @@
 print("is this really a computer program?")
 print("yes, it is. Adding a second line.")
 print("yes, it is. Adding a third line to test synchronization.")
+print("yes, it is. Adding the forth line locally to test commit back to Github.")
