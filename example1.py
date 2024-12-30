2809a48a8a76bfe265ac55aa0794a3c8692e12a2
GeorgeDealy <49003850+GeorgeDealy@users.noreply.github.com>
2024-12-30 18:11:16 -0500

    Test remove line
    Removed line 4

--- example1.py
+++ example1.py
@@ -1,4 +1,4 @@
 print("is this really a computer program?")
 print("yes, it is. Adding a second line.")
-print("yes, it is. Adding a third line to test synchronization.")
-print("yes, it is. Adding the forth line locally to test commit back to Github.")
+print("yes, it is. Adding a third line to test synchronization, and removing 4th line")
+# removing 4th line 
